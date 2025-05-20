79d7b856d56c6f1eb64f23ea87319ca1aaeff47c
Tóth Kata <toth.kata@ckik.hu>
2025-05-20 21:28:00 +0200

    uhhhh tippem nincs

--- event.js
+++ event.js
@@ -13,6 +13,7 @@ var palyapoziciok = [
     [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 ]
 var ennyitleptek = [0, 0, 0, 0]
+var vanapalyan = [false, false, false, false]
 
 function random(a, f) {
     return Math.floor(Math.random()*(f-a+1))+a;
@@ -34,6 +35,7 @@ function felrak(szin) {
     babuk.forEach(element => {
         element.addEventListener("click", function () {
             lep(this)
+            vanapalyan[i] = true
         })
     });
 }
@@ -42,7 +44,7 @@ function dob() {
     if (dobhat) {
         dobas = random(1, 6)
         console.log(dobas)
-        if ( dobas == 6) {
+        if (dobas == 6 || vanapalyan[i]) {
             felrak(szinek[i])
         }
         console.log(szinek[i])
